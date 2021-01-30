1fe3200ca353df016176aa5028166449d305ded0
Trishal Dhayananda <41100426+trishald@users.noreply.github.com>
2021-01-30 19:03:24 +0530

    added nav html

--- src/comps/Navbar.js
+++ src/comps/Navbar.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React from 'react'
 
 function Navbar() {
-  return <div className="navbar">
-
-  </div>;
+  return (
+    <div>
+    <nav>
+      <input type="checkbox" id="check"/>
+      <label for="check" class="checkbtn">
+        <i className="fas fa-bars"></i>
+      </label>
+      <label className="logo">DesignX</label>
+      <ul>
+        <li><a className="active" href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Feedback</a></li>
+      </ul>
+    </nav>  
+    </div>
+  )
 }
 
-export default Navbar;
+export default Navbar
